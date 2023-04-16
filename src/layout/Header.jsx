caf18b2006f9ab7b413f9f86427e71b824bf5159
Alex Pirko <youzhny_foto@gmail.com>
2023-04-16 11:13:54 +0300

    init: create new movie-app task

---
+++ src/layout/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return <nav className="teal darken-1">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">MoviesApp</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="!#">Sass</a></li>
+        </ul>
+      </div>
+    </nav>
+}
+export {Header};
---
+++ src/layout/Main.jsx
@@ -0,0 +1,6 @@
+function Main() {
+  return <main className="container content">
+    Hi!
+  </main>
+}
+export {Main};
