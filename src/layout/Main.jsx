e8028d0e0289942d154f3570aac4995c0a183c38
Alex Pirko <youzhny_foto@gmail.com>
2023-04-17 12:05:08 +0300

    feat: create main components

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,29 @@
-function Main() {
-  return <main className="container content">
-    Hi!
-  </main>
+import React from "react";
+import {Movies} from "../components/Movies";
+import { Preloader } from "../components/Preloader";
+
+class Main extends React.Component {
+  state = {
+    movies: []
+  }
+
+  componentDidMount() {
+    fetch('https://www.omdbapi.com/?apikey=ea8d3b58&s=avengers')
+      .then(response => response.json())
+      .then(data => this.setState({movies: data.Search}))
+  }
+  
+  render() {
+    const {movies} = this.state;
+
+    return <main className="container content">
+      {
+        movies.length ? (
+          <Movies movies={this.state.movies} />
+        ) : <Preloader />
+      }
+      
+    </main>
+  }    
 }
 export {Main};
